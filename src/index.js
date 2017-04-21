77cec522c7551aec9188d9d2d4c34abade92968b
Eric K <ejkwong@gmail.com>
2017-04-21 13:14:12 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import { Provider } from 'react-redux';
 import './index.css';
+import initializeStore from './stores/initializeStore';
+import * as actions from './actions';
 
+//dummy data for testing
+const songs = [{title: 'sample title'}, {title: 'title 2'}];
+
+//make the store
+const store = initializeStore();
+
+//make the playlist with songs
+store.dispatch(actions.setPlaylist(songs));
+
+//render
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+  <App />
+  </Provider>,
   document.getElementById('root')
 );
