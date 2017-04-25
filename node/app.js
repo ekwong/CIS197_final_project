9992c6c8a5b3b1bfaada01a4578e7ea05b084339
Eric K <ejkwong@gmail.com>
2017-04-25 13:59:24 -0400

    able to submit to authorization url and get authorization code

--- node/app.js
+++ node/app.js
@@ -1,15 +1,42 @@
 var express = require('express');
 var app = express();
 var uuid = require('node-uuid');
+var SpotifyWebApi = require('spotify-web-api-node');
+var credentials = require('./config');
 
 // Serve static pages
 app.engine('html', require('ejs').__express);
 app.set('view engine', 'html');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-  res.render('index');
+
+var scopes = ['user-read-private', 'user-read-email'];
+
+console.log(credentials.client_id);
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+  clientId: credentials.client_id,
+  clientSecret: credentials.client_secret,
+  redirectUri: credentials.callback_URL
 });
+// Create the authorization URL
+var authorizeURL = spotifyApi.createAuthorizeURL(scopes);
+
+
+app.get('/', function (req, res) {
+  res.redirect(authorizeURL);
+  console.log("AUTHORIZE URL: " + authorizeURL);
+});
+
+app.get('/callback', function (req, res) {
+  console.log("in callback");
+  if (req.query.error) {
+    console.log("error is: " + req.query.error);
+  }
+  var authCode = req.query.code;
+  console.log("AUTH CODE: " + authCode);
+  res.send(authCode);
+})
 
 // Generate a random cookie secret for this app
 var generateCookieSecret = function () {
@@ -32,19 +59,19 @@ app.use(bodyParser.urlencoded({extended: false}));
 // preceded only by necessary middleware functions.
 // DO NOT mount an 'authenticating' middleware function in a separate call to use().
 // For instance, the API routes require a valid key, so mount checkValidKey and apiRouter in the same call.
-var keysRouter = require('./routes/keys');
-app.use('/', keysRouter);
+// var keysRouter = require('./routes/keys');
+// app.use('/', keysRouter);
 
-var loginRouter = require('./routes/login');
-app.use('/', loginRouter);
+// var loginRouter = require('./routes/login');
+// app.use('/', loginRouter);
 
-var isAuthenticated = require('./middlewares/isAuthenticated');
-var reviewsRouter = require('./routes/reviews');
-app.use('/reviews', isAuthenticated, reviewsRouter);
+// var isAuthenticated = require('./middlewares/isAuthenticated');
+// var reviewsRouter = require('./routes/reviews');
+// app.use('/reviews', isAuthenticated, reviewsRouter);
 
-var checkValidKey = require('./middlewares/checkValidKey');
-var apiRouter = require('./routes/api');
-app.use('/api', checkValidKey, apiRouter);
+// var checkValidKey = require('./middlewares/checkValidKey');
+// var apiRouter = require('./routes/api');
+// app.use('/api', checkValidKey, apiRouter);
 
 var handleError = require('./middlewares/handleError');
 app.use(handleError);
